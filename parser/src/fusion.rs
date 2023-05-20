9b4599d5081ea73f172c2ab3ec09c1a2ead292d8
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2023-05-20 19:56:13 +0800

    license: move to MIT/Apache double license

--- parser/src/fusion.rs
+++ parser/src/fusion.rs
@@ -1,32 +1,10 @@
-/*
-MPL 2.0 License applies to this file with exceptions specified in 
-the Apache License notice below.
-
-Copyright (C) 2023 Paguroidea Developpers
-
-This Source Code Form is subject to the terms of the Mozilla Public
-License, v. 2.0. If a copy of the MPL was not distributed with this
-file, You can obtain one at <https://mozilla.org/MPL/2.0/>.
-*/
-
-/*
-The generated code (distributed either as token streams or in the 
-file form) is licensed under the Apache License, Version 2.0.
-
-Copyright 2023 Paguroidea Developpers
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// Copyright (c) 2023 Paguroidea Developpers
+//
+// Licensed under the Apache License, Version 2.0
+// <LICENSE-APACHE or http://www.apache.org/licenses/LICENSE-2.0> or the MIT
+// license <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. All files in the project carrying such notice may not be copied,
+// modified, or distributed except according to those terms.
 
 use std::{collections::HashMap, matches};
 
