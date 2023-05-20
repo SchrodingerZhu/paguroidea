b2984c33ea3b9a6e635f148192d3470964afe0de
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2023-05-20 18:49:27 +0800

    add license headers

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,7 +1,14 @@
+/*
+Copyright (C) 2023 Paguroidea Developpers
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at <https://mozilla.org/MPL/2.0/>.
+*/
+
 pub mod core_syntax;
 pub mod frontend;
 mod fusion;
-pub mod lexer;
 mod nf;
 pub mod type_system;
 pub mod utilities;
