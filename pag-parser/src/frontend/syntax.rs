9350d6460f4bb57cb1068c235cce450fea344ef4
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2023-05-23 16:21:34 +0800

    apply clippy fix

--- pag-parser/src/frontend/syntax.rs
+++ pag-parser/src/frontend/syntax.rs
@@ -6,7 +6,7 @@
 // option. All files in the project carrying such notice may not be copied,
 // modified, or distributed except according to those terms.
 
-use std::{collections::HashMap, todo, unimplemented};
+use std::{collections::HashMap};
 
 use crate::{
     core_syntax::BindingContext,
