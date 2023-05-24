def8d1ada07d3d1a8052a874227f3693e6929ef8
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2023-05-24 15:42:57 +0800

    ci: try to mark generated file again

--- pag-compiler/src/lib.rs
+++ pag-compiler/src/lib.rs
@@ -4,12 +4,15 @@ use syn::File;
 
 // to be used in build.rs
 pub fn compile<I: AsRef<Path>, O: AsRef<Path>>(input: I, output: O) {
+    use std::io::Write;
     let data = std::fs::read_to_string(input.as_ref()).unwrap();
     match pag_parser::generate_parser(&data) {
         Ok(tokens) => {
             let tree: File = syn::parse2(tokens).unwrap();
             let prettified = prettyplease::unparse(&tree);
-            std::fs::write(output.as_ref(), prettified).unwrap();
+            let mut file = std::fs::File::create(output.as_ref()).unwrap();
+            write!(file, "// @generated\n\n{}", prettified).unwrap();
+            file.flush().unwrap();
         }
         Err(errs) => {
             errs.report_stderr(&format!("{}", input.as_ref().display()), &data)
