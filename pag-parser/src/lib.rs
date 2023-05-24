8c776d278e0719caaf26e87889417b70d6767735
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2023-05-24 21:15:19 +0800

    fix: reduce stack footprint for err (#15)

--- pag-parser/src/lib.rs
+++ pag-parser/src/lib.rs
@@ -5,7 +5,7 @@
 // license <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
 // option. All files in the project carrying such notice may not be copied,
 // modified, or distributed except according to those terms.
-
+#![feature(box_into_inner)]
 use std::ops::Range;
 
 use ariadne::{Color, Report, ReportKind, Source};
