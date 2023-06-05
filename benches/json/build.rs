5888a5ee74103b0f54ce63672ebfe3b137e5c62d
PoloGe <119010079@link.cuhk.edu.cn>
2023-06-05 17:06:55 +0800

    feat: rustfmt
    Signed-off-by: PoloGe <119010079@link.cuhk.edu.cn>

--- benches/json/build.rs
+++ benches/json/build.rs
@@ -1,7 +1,9 @@
 extern crate lalrpop;
 
 fn main() {
-    lalrpop::Configuration::new().process_dir("benches/").unwrap();
+    lalrpop::Configuration::new()
+        .process_dir("benches/")
+        .unwrap();
     pag_compiler::compile("json.pag", "src/parser.rs");
     println!("cargo:rerun-if-changed=json.pag");
 }
