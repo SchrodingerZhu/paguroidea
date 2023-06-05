d780fe3ef82fb27ce9a5e6e1d5e80b48add338c1
PoloGe <119010079@link.cuhk.edu.cn>
2023-06-05 13:44:37 +0800

    feat: add lalrpop variation for json
    Signed-off-by: PoloGe <119010079@link.cuhk.edu.cn>

---
+++ benches/json/benches/lalrlexer.rs
@@ -0,0 +1,44 @@
+extern crate lexical;
+use std::fmt;
+
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Pvalue<'a> {
+    Number(f64),
+    String(&'a str),
+    Object(Vec<(&'a str, Pvalue<'a>)>),
+    Bool(bool),
+    Null,
+    Array(Vec<Pvalue<'a>>),
+}
+
+impl<'a> fmt::Display for Pvalue<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Pvalue::Number(float) => write!(f, "{}", lexical::to_string(*float)),
+            Pvalue::String(string) => write!(f, "\"{}\"", string),
+            Pvalue::Object(obj) => {
+                write!(f, "{{")?;
+                if let Some(((key, value), rest)) = obj.split_first() {
+                    write!(f, "\"{}\": {}", key, value)?;
+                    for (key, value) in rest.iter() {
+                        write!(f, ", \"{}\": {}", key, value)?
+                    }
+                }
+                write!(f, "}}")
+            }
+            Pvalue::Bool(flag) => write!(f, "{}", flag),
+            Pvalue::Null => write!(f, "null"),
+            Pvalue::Array(array) => {
+                write!(f, "[")?;
+                if let Some((value, rest)) = array.split_first() {
+                    write!(f, "{}", value)?;
+                    for value in rest.iter() {
+                        write!(f, ", {}", value)?
+                    }
+                }
+                write!(f, "]")
+            }
+        }
+    }
+}
--- benches/json/build.rs
+++ benches/json/build.rs
@@ -1,4 +1,7 @@
+extern crate lalrpop;
+
 fn main() {
+    lalrpop::Configuration::new().process_dir("benches/").unwrap();
     pag_compiler::compile("json.pag", "src/parser.rs");
     println!("cargo:rerun-if-changed=json.pag");
 }
