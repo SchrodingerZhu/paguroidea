4762d6e2223d53b4bff916c169162f7494fd8927
QuarticCat <QuarticCat@pm.me>
2023-06-20 00:28:10 +0800

    add dbg_sort

---
+++ pag-lexer/src/utilities.rs
@@ -0,0 +1,17 @@
+pub fn dbg_sort<T, U, F, K>(data: T, _f: F) -> impl Iterator<Item = U>
+where
+    T: IntoIterator<Item = U>,
+    F: FnMut(&U) -> K,
+    K: Ord,
+{
+    #[cfg(not(debug_assertions))]
+    {
+        data.into_iter()
+    }
+    #[cfg(debug_assertions)]
+    {
+        let mut vec = Vec::from_iter(data.into_iter());
+        vec.sort_unstable_by_key(_f);
+        vec.into_iter()
+    }
+}
