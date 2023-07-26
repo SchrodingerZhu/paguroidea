aad324d428c14c419a4830065d2ebc6036342a83
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2023-07-25 21:16:32 -0400

    print normal form table

--- pag-parser2/src/nf/mod.rs
+++ pag-parser2/src/nf/mod.rs
@@ -6,9 +6,12 @@
 // option. All files in the project carrying such notice may not be copied,
 // modified, or distributed except according to those terms.
 
+use std::{collections::HashMap, ops::Deref};
+
 use quote::format_ident;
 use syn::Ident;
 
+use crate::debug::styled;
 #[cfg(feature = "debug")]
 use crate::debug::styled_write;
 mod semact;
@@ -39,8 +42,8 @@ impl Tag {
 impl std::fmt::Display for Tag {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Tag::Toplevel(ident) => write!(f, "{}", ident),
-            Tag::Anonymous(index) => write!(f, "{{{}}}", index),
+            Tag::Toplevel(ident) => write!(f, "{ident}"),
+            Tag::Anonymous(index) => styled_write!(f, Style::new().italic(), "_{index}"),
         }
     }
 }
@@ -71,16 +74,16 @@ impl std::fmt::Display for Action {
         match self {
             Self::Reduce { tag, output } => {
                 if *output {
-                    styled_write!(f, Color::Red, "[{tag}]")
+                    styled_write!(f, Color::Blue.underline(), "{tag}")
                 } else {
-                    styled_write!(f, Color::Blue, "[{tag}]")
+                    styled_write!(f, Color::Blue, "{tag}")
                 }
             }
             Self::Shift { tag, output } => {
                 if *output {
-                    styled_write!(f, Color::Red, "{tag}")
+                    styled_write!(f, Color::Red.underline(), "{tag}")
                 } else {
-                    styled_write!(f, Color::Blue, "{tag}")
+                    styled_write!(f, Color::Red, "{tag}")
                 }
             }
         }
@@ -102,22 +105,22 @@ impl std::fmt::Display for NormalForm {
                 write!(f, "ε")?;
                 for (tag, output) in actions.iter() {
                     if *output {
-                        styled_write!(f, Color::Red, "[{tag}]")?;
+                        styled_write!(f, Color::Blue.underline(), "\t{tag}")?;
                     } else {
-                        styled_write!(f, Color::Blue, "[{tag}]")?;
+                        styled_write!(f, Color::Blue, "\t{tag}")?;
                     }
                 }
             }
             Self::Unexpanded(actions) => {
                 write!(f, "{}", actions[0])?;
                 for action in &actions[1..] {
-                    write!(f, " {}", action)?;
+                    write!(f, "\t{}", action)?;
                 }
             }
             Self::Sequence(terminal, actions) => {
-                styled_write!(f, Color::Yellow.bold(), "{terminal}")?;
+                styled_write!(f, Color::Yellow, "{terminal}")?;
                 for action in actions.iter() {
-                    write!(f, " {}", action)?;
+                    write!(f, "\t{}", action)?;
                 }
             }
         }
@@ -151,4 +154,87 @@ fn debug_print_test() {
         ],
     );
     println!("{}", sequence);
+}
+
+/// Well, it is not the notorius firewall.
+pub struct NFTable(HashMap<Tag, Vec<NormalForm>>);
+
+impl Deref for NFTable {
+    type Target = HashMap<Tag, Vec<NormalForm>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+#[cfg(feature = "debug")]
+impl std::fmt::Display for NFTable {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let width = term_size::dimensions().map(|x| x.0).unwrap_or(0);
+        writeln!(f, "┏{}┓", "━".repeat(width.saturating_sub(2)))?;
+        writeln!(
+            f,
+            "\t{}\t{}\t{}\t{}\n",
+            styled!(Color::Red.bold(), "Shift"),
+            styled!(Color::Blue.bold(), "Reduce"),
+            styled!(Style::new().underline().bold(), "Output"),
+            styled!(Style::new().italic().bold(), "Anonymous"),
+        )?;
+        for (tag, forms) in self.iter() {
+            writeln!(
+                f,
+                "\t{}\t=\t{}",
+                styled!(Style::new().underline(), "{tag}"),
+                forms[0]
+            )?;
+            for form in &forms[1..] {
+                writeln!(f, "\t\t|\t{}", form)?;
+            }
+            writeln!(f)?;
+        }
+        writeln!(f, "┗{}┛", "━".repeat(width.saturating_sub(2)))
+    }
+}
+
+#[cfg(all(feature = "debug", test))]
+#[test]
+fn debug_print_nf_table() {
+    use quote::format_ident;
+    let sequence = NormalForm::Sequence(
+        format_ident!("TEST"),
+        vec![
+            Action::Shift {
+                tag: Tag::Toplevel(format_ident!("a")),
+                output: false,
+            },
+            Action::Reduce {
+                tag: Tag::Toplevel(format_ident!("b")),
+                output: true,
+            },
+            Action::Shift {
+                tag: Tag::Toplevel(format_ident!("c")),
+                output: true,
+            },
+            Action::Reduce {
+                tag: Tag::Anonymous(1),
+                output: false,
+            },
+        ],
+    );
+    let empty = NormalForm::Empty(vec![
+        (Tag::Toplevel(format_ident!("a")), false),
+        (Tag::Toplevel(format_ident!("b")), true),
+    ]);
+    let table = NFTable(
+        vec![
+            (
+                Tag::Toplevel(format_ident!("TEST1")),
+                vec![sequence.clone(), empty.clone()],
+            ),
+            (Tag::Toplevel(format_ident!("TEST2")), vec![sequence, empty]),
+        ]
+        .into_iter()
+        .collect(),
+    );
+    println!("{}", table);
 }
