219e02a10ded6e922e30c061cf1d4fe9a94203c4
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2023-07-28 00:36:55 -0400

    stage work for translation

--- pag-parser2/src/nf/translation.rs
+++ pag-parser2/src/nf/translation.rs
@@ -7,9 +7,9 @@ use std::collections::HashMap;
 use quote::format_ident;
 use syn::{Ident, Type};
 
-use crate::frontend::ParserDef;
+use crate::frontend::{ParserDef, ParserExpr};
 
-use super::{semact::SemAct, NormalForm, Tag};
+use super::{semact::SemAct, NormalForm, Tag, Action};
 
 struct Translation {
     /// Table of semi-normalized production rules
@@ -37,6 +37,56 @@ impl Translation {
         self.anonymous_cnt += 1;
         result
     }
+    fn construct_actions_from_expr_sequence<'a, I>(&mut self, stream: I) -> Vec<Action> 
+    where
+        I: Iterator<Item = (&'a ParserExpr, Option<Ident>)>,
+    {
+        stream.map(|(expr, output )| {
+            match expr {
+                ParserExpr::ParserRef(rule) => 
+                    Action::Shift { tag: Tag::Toplevel(rule.clone()), output: output.or_else(|| Some(self.new_output_sym())) },
+                ParserExpr::LexerRef(_)  => 
+                    Action::Shift { tag: self.add_anonymous_rule(expr), output }, 
+                ParserExpr::Ignore(inner)  => 
+                    Action::Shift { tag: self.add_anonymous_rule(inner), output: None }, 
+                ParserExpr::Hinted( inner, ty) => {
+                    let tag = self.add_anonymous_rule(inner);
+                    self.hints.insert(tag.clone(), ty.clone());
+                    Action::Shift { tag, output: None }
+                }
+                _ => 
+                    Action::Shift { tag: self.add_anonymous_rule(expr), output: output.or_else(|| Some(self.new_output_sym())) },    
+            }
+        }).collect()
+    }
+    fn construct_nf_from_expr_sequence<'a, I>(&mut self, mut stream: I, semact: SemAct) -> NormalForm
+    where
+        I: Iterator<Item = (&'a ParserExpr, Option<Ident>)>,
+    {
+        let head = stream.next();
+        match head {
+            None => NormalForm::Empty(vec![], semact),
+            // Token rule is ignored on default, but can be used to specify the label.
+            Some((ParserExpr::LexerRef(token), label)) => {
+                let actions = self.construct_actions_from_expr_sequence(stream);
+                NormalForm::Sequence(token.clone(), label, actions, semact)
+            }
+            Some(_) => {
+                let recovered = head.into_iter().chain(stream);
+                let actions = self.construct_actions_from_expr_sequence(recovered);
+                NormalForm::Unexpanded(actions, semact)
+            }
+        }
+    }
+
+    fn add_anonymous_rule(&mut self, expr: &ParserExpr) -> Tag {
+        // Must be primitive rules
+
+        let tag = self.new_anonymous_tag();
+        let semact = SemAct::infer(expr);
+        
+    }
+
     // Translate a top-level definition
     fn add_toplevel_def(&mut self, name: Ident, def: &ParserDef) {
         let tag = Tag::Toplevel(name);
