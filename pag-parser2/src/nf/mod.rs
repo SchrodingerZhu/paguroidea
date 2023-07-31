e2334262af904ef802e8bdac963d4faae660f2b4
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2023-07-31 13:47:49 -0400

    address formatting issues

--- pag-parser2/src/nf/mod.rs
+++ pag-parser2/src/nf/mod.rs
@@ -5,9 +5,9 @@
 // license <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
 // option. All files in the project carrying such notice may not be copied,
 // modified, or distributed except according to those terms.
+mod normalization;
 mod semact;
 mod translation;
-mod normalization;
 
 use crate::utils::Appendix;
 
--- pag-parser2/src/nf/semact.rs
+++ pag-parser2/src/nf/semact.rs
@@ -6,7 +6,7 @@
 // option. All files in the project carrying such notice may not be copied,
 // modified, or distributed except according to those terms.
 
-use crate::frontend::{CodeBlock, ParserExpr};
+use crate::frontend::CodeBlock;
 
 ///
 /// ```
